879a367a1630d0f94de0f71b3951e42d9f02537f
Vadim <russianspiritoneshotlol@gmail.com>
2022-05-18 17:33:25 +0300

    4th task complited (Wed, May 18, 2022 05:33:25 PM)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,24 @@
+const path = require('path');
+const fs = require('fs/promises');
+const dir = path.join(__dirname, 'files');
+const dirCopy = path.join(__dirname, 'files-copy');
 
+fs.rm(dirCopy, {
+  recursive:true,
+  force: true
+}).finally(() => {
+  fs.mkdir(dirCopy, {
+    recursive: true
+  });
+  fs.readdir(dir, {
+    withFileTypes:true
+  }).then((data) => {
+    data.forEach((item) => {
+      if (item.isFile()) {
+        const pathItem = path.join(dir, item.name);
+        const pathItemCopy = path.join(dirCopy, item.name);
+        fs.copyFile(pathItem, pathItemCopy);
+      }
+    });
+  });
+});
